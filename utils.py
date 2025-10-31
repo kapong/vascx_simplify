dec0f6ecdbdc8779f64b2607df3f10fe261bf9f6
Phongphan Phienphanich <garpong@gmail.com>
2025-10-31 23:25:20 +0700

    remove outside code

--- utils.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import hf_hub_download
-
-def from_huggingface(modelstr: str):
-    repo_name, repo_fpath = modelstr.split(":")
-    fpath = hf_hub_download(repo_id=repo_name, filename=repo_fpath)
-    return fpath
