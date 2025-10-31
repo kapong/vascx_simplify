4545b6f2d04362071bf9fa573e40dc1a18cf94f1
Phongphan Phienphanich <garpong@gmail.com>
2025-11-01 02:56:02 +0700

    chore: bump version to 0.1.4

--- src/vascx_simplify/__init__.py
+++ src/vascx_simplify/__init__.py
@@ -4,7 +4,7 @@ This is a simplified rewrite of the original rtnls_vascx_models project.
 Original work: https://github.com/Eyened/rtnls_vascx_models
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .inference import (
     ClassificationEnsemble,
