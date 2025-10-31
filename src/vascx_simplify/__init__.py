a873f20f8a0e9d845273921751453f857ca2d50a
Phongphan Phienphanich <garpong@gmail.com>
2025-11-01 00:13:58 +0700

    Bump version to 0.1.2

--- src/vascx_simplify/__init__.py
+++ src/vascx_simplify/__init__.py
@@ -4,7 +4,7 @@ This is a simplified rewrite of the original rtnls_vascx_models project.
 Original work: https://github.com/Eyened/rtnls_vascx_models
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 from .inference import (
     sliding_window_inference,
